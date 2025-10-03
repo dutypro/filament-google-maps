15734f69b3919683239cc713417eb8f633aae893
dutypro <38077637+dutypro@users.noreply.github.com>
2025-10-03 14:56:24 -0700

    refactor: clean up HTML formatting in Google Maps entry blade template

--- resources/views/infolists/filament-google-maps-entry.blade.php
+++ resources/views/infolists/filament-google-maps-entry.blade.php
@@ -3,33 +3,26 @@
         $statePath = $getStatePath();
     @endphp
 
-    <div
-        x-ignore
-        ax-load
-        ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-google-maps-entry', 'cheesegrits/filament-google-maps') }}"
+    <div x-ignore x-load
+        x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-google-maps-entry', 'cheesegrits/filament-google-maps') }}"
         x-data="filamentGoogleMapsField({
-                    state: @js($getState()),
-                    defaultLocation: @js($getDefaultLocation()),
-                    controls: @js($getMapControls(false)),
-                    layers: @js($getLayers()),
-                    defaultZoom: @js($getDefaultZoom()),
-                    drawingField: @js($getDrawingField()),
-                    geoJson: @js($getGeoJsonFile()),
-                    geoJsonVisible: @js($getGeoJsonVisible()),
-                    gmaps: @js($getMapsUrl()),
-                    mapEl: $refs.map,
-                })"
-        id="{{ $getId() . '-alpine' }}"
-        wire:ignore
-    >
-        <div
-            x-ref="map"
-            class="w-full"
+            state: @js($getState()),
+            defaultLocation: @js($getDefaultLocation()),
+            controls: @js($getMapControls(false)),
+            layers: @js($getLayers()),
+            defaultZoom: @js($getDefaultZoom()),
+            drawingField: @js($getDrawingField()),
+            geoJson: @js($getGeoJsonFile()),
+            geoJsonVisible: @js($getGeoJsonVisible()),
+            gmaps: @js($getMapsUrl()),
+            mapEl: $refs.map,
+        })" id="{{ $getId() . '-alpine' }}" wire:ignore>
+        <div x-ref="map" class="w-full"
             style="
                 height: {{ $getHeight() }};
                 min-height: 30vh;
                 z-index: 1 !important;
-            "
-        ></div>
+            ">
+        </div>
     </div>
 </x-dynamic-component>
